4b63354d5fc491935ad3b8be3b9d84ea2f972a09
jakaria6284 <kamal.jakaria.6284mia@gmail.com>
2024-01-20 14:35:49 +0600

    first commit

---
+++ app/src/main/java/com/ariyanjakaria/lockquize/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ariyanjakaria.lockquize;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+}
